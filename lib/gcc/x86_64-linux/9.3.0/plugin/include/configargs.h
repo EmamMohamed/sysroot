74171d5cf4eed454311c96719f7144c0b9c50f75
Mohamed Emam <Mohamed.Emam@mercedes-benz.com>
2024-07-30 15:43:19 +0200

    sysroot files

---
+++ lib/gcc/x86_64-linux/9.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --build=x86_64-linux-gnu --host=x86_64-linux --target=x86_64-linux --enable-default-pie --enable-languages=c,c++,fortran --disable-multilib --prefix=/var/buildlibs/gcc --enable-libstdcxx-threads --with-build-sysroot=/var/builds/sysroot --with-sysroot=/var/builds/sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
